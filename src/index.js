09e102f976ad1264bdb1d953121ee0f1db19cd59
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-08-10 15:27:46 -0400

    criando um novo código de email e senha

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
-// Validando email
+// Pegando os valores de email e senha
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let form =  document.querySelector('form');
 
-function validacaoEmail(field) {
-    //Aqui estamos extraindo o nome de usuário de um endereço de email.
-    usuario = field.value.substring(0, field.value.indexOF("@"));
-    dominio = field.value.substring(field.value.indexOF("@")+ 1, field.value.length);
+form.addEventListener("submit", (e) => {
+  
 
-    if((usuario.length >= 1) &&
-      (dominio.length >=3 )&&
-      (usuario.search("@")==-1) &&
-      (dominio.search("@")==-1) &&
-      (usuario.search(" ")==-1) &&
-      (dominio.search(" ")==-1) &&
-      (dominio.search(".")!=1) &&
-      (dominio.indexOF(".")>=1)&&
-      (dominio.lastIndeOF(".") < dominio.length -1)) {
-        document.getElementById("msgEmail").innerHTML="E-mail válido";
-        alert("E-mail válido");
-    }
-    else{
-      document.getElementById("msgEmail").innerHTML="<font color='red'>E-mail inválido</font>"
-        alert("E-mail inválido");
-    }
-}
+  console.log(email.value)
+  console.log(password.value)
+  e.preventDefault()
+})
