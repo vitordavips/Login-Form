6b6c82a908efe2be65524da42ad1e4e4d03c37bd
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-08-10 17:01:30 -0400

    terminando o código  para validar o email

--- src/index.js
+++ src/index.js
@@ -2,29 +2,32 @@
 let email = document.getElementById('email');
 let password = document.getElementById('password');
 let form =  document.querySelector('form');
-let textForm = document.getElementById('textForm');
+let textEmail = document.getElementById('textEmail');
 
 form.addEventListener("submit", (e) => {
   // criando uma condição para não enviar valores vazios.
   if(email.value == '' && password.value == '') {
     alert('Você precisa preencher todos os campos!')
-  }
-  else{
+  }else{
     console.log(email.value)
     console.log(password.value)
   }
   e.preventDefault()
 })
 
-email.addEventListener("keyup", () => {
+// Aqui verefica se o email foi escrito corretamente.
+email.addEventListener("blur", () => {
   if(validarEmail(email.value) !== true){
     alert("O formato do email deve ser Ex: abcd@gmail.com")
+  }else{
+    textEmail.innerHTML ='';
   }
 })
 
 //criando a função para validar o email.
-function validarEmail(){
+function validarEmail(email) {
   // Aqui vai está os parâmetros para validar o emali.
   let emailParametros = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-  return emailParametros.test(email)
+  return emailParametros.test(email);
 }
+
